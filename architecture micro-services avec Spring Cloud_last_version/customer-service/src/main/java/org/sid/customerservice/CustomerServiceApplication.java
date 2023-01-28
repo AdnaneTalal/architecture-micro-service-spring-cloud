7a40b9e7a1a69a4ddacd587d890c5d54056d6650
adnane Talal <101368864+AdnaneTalal@users.noreply.github.com>
2023-01-28 03:38:20 +0100

    Update CustomerServiceApplication.java

--- architecture micro-services avec Spring Cloud_last_version/customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ architecture micro-services avec Spring Cloud_last_version/customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,35 +1,44 @@
-package org.sid.customerservice;
+package com.emsi.customerservice;
 
-import org.sid.customerservice.entities.Customer;
-import org.sid.customerservice.repositories.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.emsi.customerservice.entities.Customer;
+import com.emsi.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CustomerServiceApplication {
 
+	private Logger loging;
 
-    public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
 
-    @Bean 
-    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
 
-        restConfiguration.exposeIdsFor(Customer.class);
-        return args -> {
-            customerRepository.save(new Customer(null,"adnane","adnane@gmail.com"));
-            customerRepository.save(new Customer(null,"talal","talal@gmail.com"));
-            customerRepository.save(new Customer(null,"john","john@gmail.com"));
+		restConfiguration.exposeIdsFor(Customer.class);
+		return args -> {
+			customerRepository.save(new Customer(null,"adnane","adnane@gmail.com"));
+			customerRepository.save(new Customer(null,"talal","talal@gmail.com"));
+			customerRepository.save(new Customer(null,"john","john@gmail.com"));
 
-            customerRepository.findAll().forEach(c->{
-                System.out.println(c.toString());
-            });
-        };
-    }
+// log messages using log(Level level, String msg)
+			loging= Logger.getLogger(CustomerServiceApplication.class.getName());
+			customerRepository.findAll().forEach(c->
+					loging.log(Level.INFO, c.toString()));
 
+
+
+
+
+
+		};
+	}
 }
